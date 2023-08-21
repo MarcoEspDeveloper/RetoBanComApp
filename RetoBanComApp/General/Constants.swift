b9419703d76bb1a04e187d86230ed43ae049ca44
Marco Antonio Espinal <>
2023-08-20 21:26:15 -0500

    - Add main VIPER project structure. - Add initial example login view controller.

---
+++ RetoBanComApp/General/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  RetoBanComApp
+//
+//  Created by Marco Antonio  on 20/08/23.
+//
+
+import Foundation
--- RetoBanComApp/Modules/Login/View/LoginViewController.swift
+++ RetoBanComApp/Modules/Login/View/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  RetoBanComApp
 //
 //  Created by Marco Antonio  on 20/08/23.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
