8554d9d16fa414963c7f3c3995ead090947fb054
Marco Antonio Espinal <>
2023-08-22 14:18:41 -0500

    Add users list connection logic.

--- RetoBanComApp/Data/Connection/ConnectionManager.swift
+++ RetoBanComApp/Data/Connection/ConnectionManager.swift
@@ -12,7 +12,40 @@ class ConnectionManager: ConnectionManagerProtocol {
     
     func getUsersList(page: Int, handler: @escaping ([UserResponse]?, NSError?) -> Void) {
         
-        
+        let url = "\(Constants.ConnectionUrl.baseUrl)\(Constants.ConnectionUrl.usersUrl)"
+            
+        AF.session.configuration.timeoutIntervalForRequest = 10
+
+        AF.request(url).validate().responseDecodable(of: [UserResponse].self) { (response) in
+            
+            switch response.result {
+            case .success(let value):
+                handler(value, nil)
+            case .failure(let error):
+                
+                var errorCode = 0
+                
+                if let underlyingError = error.asAFError?.underlyingError {
+                    if let urlError = underlyingError as? URLError {
+                        switch urlError.code {
+                        case .timedOut:
+                            errorCode = urlError.code.rawValue
+                            print("TIMED OUT ERROR")
+                        case .notConnectedToInternet:
+                            errorCode = urlError.code.rawValue
+                            print("INTERNET ERROR")
+                        default:
+                            //Do something
+                            errorCode = urlError.code.rawValue
+                        }
+                    }
+                }
+                
+                let newError = NSError(domain: url, code: (errorCode), userInfo:[NSLocalizedDescriptionKey:error.localizedDescription])
+                    
+                handler(nil, newError)
+            }
+        }
     }
     
     
